77e456ef8babc4fa5adddfb6a932353db09c155f
Myiah Moore <myiahsade@gmail.com>
2018-07-24 12:03:15 -0500

    check in

---
+++ src/EX22.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class EX22 {
+
+    public static void main(String[] args)
+    {
+        ArrayList<String> squad = new ArrayList<String>();
+        squad.add ("Cat");
+        squad.add ("Dog");
+        squad.add ("Cow");
+        squad.add ("Chicken");
+        System.out.println(squad);
+    }
+}
---
+++ src/EX23.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class EX23 {
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(15);
+        nums.add(30);
+        nums.add(38);
+        nums.add(45);
+        nums.add(47);
+        System.out.print(nums);
+
+    }
+}
